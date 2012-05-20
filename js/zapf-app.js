28eac4a8a5c8f20548008134bb2ae161102ee3a6
Philipp Klaus <philipp.klaus@gmail.com>
2012-05-20 20:12:02 +0200

    Viele Verbesserungen:
    - Verantwortlicher für die AKe hinzugefügt
    - Format für AK Blöcke: Bessere Raumnutzung
    - Tablet- und Handy-App zeigt Hinweis-Text nicht an
    - Icons für Zeitblöcke und Arbeitskreise

--- js/zapf-app.js
+++ js/zapf-app.js
@@ -6,22 +6,25 @@ function zeigeArbeitskreise(){
     success: function(data) {
       $("#arbeitskreiscontainer").empty();
       $.each(data.slots , function(i,item){
-        var css_id = 'slot'+item.id;
-        var start = new Date(item.begin);
-        var end = new Date(item.end);
-        var row_css_id = "row" + Math.floor(i/3);
-        // 3-spaltiges Layout (auf großen Bildschirmen)
-        if (i % 3 == 0) {
+        var css_id = 'slot'+item.id
+           ,section_id = 'slot-'+item.id
+           ,start = new Date(item.begin)
+           ,end = new Date(item.end)
+           ,num_cols = 2
+           ,row_css_id = "row" + Math.floor(i/num_cols);
+        // 2-spaltiges Layout (auf großen Bildschirmen)
+        if (i % num_cols == 0) {
           $("<div/>", { id: row_css_id, class: "row"} ).appendTo("#arbeitskreiscontainer");
         }
         // Navigations-Eintrag hinzufügen:
-        $("<div/>", { id: css_id, class: 'ak-unit span4'} ).appendTo('#'+row_css_id);
         var li = $("<li/>");
-        $("<a/>", { href: "#"+css_id, text: item.shortname }).appendTo(li)
-        li.appendTo("ul.nav");
-        $("<a/>", { name: "#"+css_id }).appendTo(css_id)
+        $("<a/>", { href: "#"+section_id, text: item.shortname }).appendTo(li)
+        li.appendTo("ul.aknav");
         // Slot zum Seiteninhalt hinzufügen:
-        $("<h2/>", { class: 'slotname', text: item.name }).appendTo('#'+css_id);
+        $("<section/>", { id: section_id}).appendTo('#'+row_css_id)
+        $("<div/>", { id: css_id+'cont', class: 'ak-unit-container'} ).appendTo('#'+section_id);
+        $("<div/>", { id: css_id, class: 'ak-unit span4'} ).appendTo('#'+css_id+'cont');
+        $("<h2/>", { class: 'slotname', html: '<i class="icon-tasks"></i>'+item.name }).appendTo('#'+css_id);
         $("<div/>", { class: 'slottime', text: wochentag(start) + " von " + $.format.date(start,'HH:mm') + ' bis ' + $.format.date(end,'HH:mm') + ' Uhr' }).appendTo('#'+css_id);
         $.format.date(new Date(), 'dd M yy')
       });
@@ -30,9 +33,11 @@ function zeigeArbeitskreise(){
         var slot_css_id = 'slot'+item.slotid;
         // Arbeitskreis zum richtigen Slot hinzufügen:
         $("<p/>", { id: css_id, class: 'ps'} ).appendTo('#'+slot_css_id);
-        $("<h3/>", { class: 'akname', text: 'AK '+item.name }).appendTo('#'+css_id);
-        $("<a/>", { class: 'aklink btn btn-info', href: item.url, text: 'Infos im Wiki'}).appendTo('#'+css_id);
+        $("<h3/>", { class: 'akname', html: '<i class="icon-chevron-right"></i> AK '+item.name }).appendTo('#'+css_id);
+        $("<div/>", { class: 'responsible', text: 'Leiter: ' + item.responsible}).appendTo('#'+css_id);
         $("<div/>", { class: 'room', text: 'Raum: ' + item.room }).appendTo('#'+css_id);
+        $("<a/>", { class: 'aklink btn-mini btn-info', href: item.url, text: 'Infos im Wiki'}).appendTo('#'+css_id);
+        $("<div/>", { class: 'clearboth'}).appendTo('#'+css_id);
       });
     },
     error: errorOccured,

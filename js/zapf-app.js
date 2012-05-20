b1861741cf8125f7457f86a3892e3ebe4e7634fd
Philipp Klaus <philipp.klaus@gmail.com>
2012-05-20 13:03:24 +0200

    Viele Änderungen:
    - Mehr Beispiel AKs
    - Besseres Layout

--- js/zapf-app.js
+++ js/zapf-app.js
@@ -2,7 +2,7 @@ function zeigeArbeitskreise(){
   $.ajax( {
     url: "api/arbeitskreise",
     dataType: 'json',
-    timeout: 2000,
+    timeout: 12000,
     success: function(data) {
       $("#arbeitskreiscontainer").empty();
       $.each(data.slots , function(i,item){
@@ -10,21 +10,29 @@ function zeigeArbeitskreise(){
         var start = new Date(item.begin);
         var end = new Date(item.end);
         var row_css_id = "row" + Math.floor(i/3);
+        // 3-spaltiges Layout (auf großen Bildschirmen)
         if (i % 3 == 0) {
           $("<div/>", { id: row_css_id, class: "row"} ).appendTo("#arbeitskreiscontainer");
         }
-        $("<div/>", { id: css_id, class: 'span4'} ).appendTo('#'+row_css_id);
+        // Navigations-Eintrag hinzufügen:
+        $("<div/>", { id: css_id, class: 'ak-unit span4'} ).appendTo('#'+row_css_id);
+        var li = $("<li/>");
+        $("<a/>", { href: "#"+css_id, text: item.shortname }).appendTo(li)
+        li.appendTo("ul.nav");
+        $("<a/>", { name: "#"+css_id }).appendTo(css_id)
+        // Slot zum Seiteninhalt hinzufügen:
         $("<h2/>", { class: 'slotname', text: item.name }).appendTo('#'+css_id);
-        $("<span/>", { class: 'slottime', text: wochentag(start) + ": on " + $.format.date(start,'HH:mm') + ' bis ' + $.format.date(end,'HH:mm') + ' Uhr' }).appendTo('#'+css_id);
+        $("<div/>", { class: 'slottime', text: wochentag(start) + " von " + $.format.date(start,'HH:mm') + ' bis ' + $.format.date(end,'HH:mm') + ' Uhr' }).appendTo('#'+css_id);
         $.format.date(new Date(), 'dd M yy')
       });
       $.each(data.arbeitskreise, function(i,item){
         var css_id = 'arbeitskreis'+i;
         var slot_css_id = 'slot'+item.slotid;
+        // Arbeitskreis zum richtigen Slot hinzufügen:
         $("<p/>", { id: css_id, class: 'ps'} ).appendTo('#'+slot_css_id);
-        $("<h3/>", { class: 'akname', text: item.name }).appendTo('#'+css_id);
+        $("<h3/>", { class: 'akname', text: 'AK '+item.name }).appendTo('#'+css_id);
         $("<a/>", { class: 'aklink btn btn-info', href: item.url, text: 'Infos im Wiki'}).appendTo('#'+css_id);
-        $("<span/>", { class: 'room', text: 'Raum: ' + item.room }).appendTo('#'+css_id);
+        $("<div/>", { class: 'room', text: 'Raum: ' + item.room }).appendTo('#'+css_id);
       });
     },
     error: errorOccured,

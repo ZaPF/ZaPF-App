767dd6a743006c7707df19d34b4eb27dbb686889
Philipp Klaus <philipp.klaus@gmail.com>
2012-05-21 14:10:39 +0200

    Links für die Arbeitskreise nun optional

--- js/zapf-app.js
+++ js/zapf-app.js
@@ -39,7 +39,7 @@ function zeigeArbeitskreise(){
         $("<h3/>", { class: 'akname', html: '<i class="icon-chevron-right"></i> AK '+item.name }).appendTo('#'+css_id);
         $("<div/>", { class: 'responsible', text: 'Leiter: ' + item.responsible}).appendTo('#'+css_id);
         $("<div/>", { class: 'room', text: 'Raum: ' + item.room }).appendTo('#'+css_id);
-        $("<a/>", { class: 'aklink btn-mini btn-info', href: item.url, text: 'Infos im Wiki'}).appendTo('#'+css_id);
+        if (item.url != null) $("<a/>", { class: 'aklink btn-mini btn-info', href: item.url, text: 'Infos im Wiki'}).appendTo('#'+css_id);
         $("<div/>", { class: 'clearboth'}).appendTo('#'+css_id);
       });
       // Anzeigen, wie viele Zeitslots bereits abgeschlossen sind:
